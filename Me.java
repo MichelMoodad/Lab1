bb5b2b02e6f41791bf7f28ba5aa3745850fbe92b
Moodad <michel.moodad@eng.lau.edu.lb>
2023-01-24 21:04:56 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -1,6 +1,7 @@
 public class Me{
 	public static void main(String[] args){
 		System.out.println("Name:Michel Moodad"+" Age: 18 years old"+" Major: Computer Engineer");
+		System.out.println("Year of study: first year");
 	}
 
 }
